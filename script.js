172ec4d6a53139d043e729a4e183c40770776e0c
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-25 16:00:10 +0200

    Change a few colors

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ scissors.addEventListener('click', () => {
 const situation = document.createElement("div");
 const result = document.createElement("div");
 
-situation.setAttribute("style", "color: rgb(188, 182, 182); font-size: 36px; text-align: center; margin-bottom: 50px;");
+situation.setAttribute("style", "color: rgb(51, 227, 227); font-size: 40px; text-align: center; margin-bottom: 50px;");
 
 function finalResult() {
     body.appendChild(result);
@@ -60,7 +60,7 @@ function finalResult() {
         result.setAttribute("style", "font-size: 70px; text-align: center; color: green;");
 
     } else if (humanScore < computerScore) {
-        result.textContent = (`You lost ${humanScore}-${computerScore} :(`);
+        result.textContent = (`You lost ${humanScore}-${computerScore}.`);
         result.setAttribute("style", "font-size: 70px; text-align: center; color: red;");
     } else {
         result.textContent = (`The match ended in a tie ${humanScore}-${computerScore}.`);
@@ -73,8 +73,8 @@ function addRefreashButton() {
     const refreash = document.createElement("button");
     body.appendChild(refreashContainer);
     refreashContainer.appendChild(refreash);
-    refreash.textContent = "Play a new match";
-    refreash.setAttribute("style", "color: green; margin-top: 50px; display: flex; justify-content: center; align-items: center; width: 200px; height: 60px; background-color: yellow; border: solid 2px white; border-radius: 10px; font-size: 20px; font-weight: bold;");
+    refreash.textContent = "New match";
+    refreash.setAttribute("style", "color: green; margin-top: 50px; display: flex; justify-content: center; align-items: center; width: 150px; height: 60px; background-color: yellow; border: solid 2px white; border-radius: 10px; font-size: 20px; font-weight: bold;");
     refreash.addEventListener("click", () => {
         location.reload();
     });
@@ -106,33 +106,33 @@ function addRefreashButton() {
         body.appendChild(situation);
         if (humanChoice === "rock") {
             if (computerChoice === "rock") {
-                situation.textContent = "It's a tie!";
+                situation.textContent = "It's a tie.";
             } else if (computerChoice === "scissors") {
                 humanScore++;
-                situation.textContent = "You win! Rock beats scissors!"
+                situation.textContent = "You win! Rock beats scissors."
             } else {
                 computerScore++;
-                situation.textContent = "You lose! Rock loses to paper!";
+                situation.textContent = "You lose. Rock loses to paper.";
             }
         } else if (humanChoice === "paper") {
             if (computerChoice === "paper") {
-                situation.textContent = "It's a tie!";
+                situation.textContent = "It's a tie.";
             } else if (computerChoice === "rock") {
                 humanScore++;
-                situation.textContent = "You win! Paper beats rock!";
+                situation.textContent = "You win! Paper beats rock.";
             } else {
                 computerScore++;
-                situation.textContent = "You lose! Paper loses to scissors!";
+                situation.textContent = "You lose. Paper loses to scissors.";
             }
         } else {
             if (computerChoice === "scissors") {
-                situation.textContent = "It's a tie!";
+                situation.textContent = "It's a tie.";
             } else if (computerChoice === "paper") {
                 humanScore++;
-                situation.textContent = "You win! Scissors beats paper!";
+                situation.textContent = "You win! Scissors beats paper.";
             } else {
                 computerScore++;
-                situation.textContent = "You lose! Scissors loses to rock!";
+                situation.textContent = "You lose. Scissors loses to rock.";
             }
         }
     }
